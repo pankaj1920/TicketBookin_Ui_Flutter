69ccaba039fa4c6b5c8df9333d41069e32ec9c74
Pankaj Bohra <pankaj.bohra@applaunch.io>
2023-02-23 10:09:44 +0530

    appbar

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,24 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Tickets"),
+      ),
+      body: const Center(child: Text("This is body")),
+    );
+  }
+}
